bdae3f3ac7a5ac7d20776e8fc4b66c02c2fe5040
zhiming.sun <sunzhiming@mxnavi.com>
2022-03-15 15:09:59 +0800

    up shutdown

--- shutdown.go
+++ shutdown.go
@@ -8,6 +8,11 @@ import (
 // Shutdown returns all the singals that are being watched for to shut down services.
 func Shutdown() []os.Signal {
 	return []os.Signal{
-		syscall.SIGHUP, syscall.SIGINT, syscall.SIGQUIT, syscall.SIGSTOP, syscall.SIGKILL, syscall.SIGTERM,
+		syscall.SIGHUP,  // 终端连接断开
+		syscall.SIGINT,  // ctrl + C
+		syscall.SIGQUIT, // ctrl + /
+		syscall.SIGTERM, // kill pid
+		// syscall.SIGKILL, // kill -9 pid // 不可捕获，立即退出
+		// syscall.SIGSTOP, // 不可捕获
 	}
 }
